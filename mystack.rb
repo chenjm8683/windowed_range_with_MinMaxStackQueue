0c2cfb05582a7eb1c107a4d3bc5d2e2137060047
Jingming Chen <chenjm8683@gmail.com>
2015-11-08 22:59:08 -0800

    finished mystack with pop, push, peek, size, empty? methods

--- mystack.rb
+++ mystack.rb
@@ -2,4 +2,24 @@ class MyStack
   def initialize
     @store = []
   end
+
+  def pop
+    @store.delete_at(-1)
+  end
+
+  def push(el)
+    @store << el
+  end
+
+  def peek
+    @store.last
+  end
+
+  def size
+    @store.length
+  end
+
+  def empty?
+    size == 0
+  end
 end
